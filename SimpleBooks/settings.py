fc0db63cfba33b14747de99b3c38ca95f5345c17
Marcin Kawalerowicz <marcin@kawalerowicz.net>
2015-05-29 13:18:25 +0200

    Crispy forms are out

--- SimpleBooks/settings.py
+++ SimpleBooks/settings.py
@@ -131,7 +131,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
-    'crispy_forms',
+    #'crispy_forms',
 	'django.contrib.humanize',
 	'app',    
     # Uncomment the next line to enable admin documentation:
